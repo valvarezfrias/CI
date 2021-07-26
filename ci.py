6494be88300533846a623e3d4278d938fa0604a6
Veronica Alvarez <valvarezfrias@eou.edu>
2021-07-26 22:20:26 +0000

    second

--- ci.py
+++ ci.py
@@ -1,9 +1,12 @@
 def add(x, y):
-  return x+y
+    return x + y
+
 
 def main():
-  x=5
-  y=2
-  print(add(5,2))
+    x = 5
+    y = 2
+    print(add(5,2))
+
+
 if __name__ == '__main__':
-  main()
+    main()
